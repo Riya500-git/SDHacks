b3ffb5843acf8379b9188630d4402fab6e7a1dd9
TRucker20 <83630849+TRucker20@users.noreply.github.com>
2022-04-10 03:00:44 -0700

    Update mageAiAPI.js

--- mageAiAPI.js
+++ mageAiAPI.js
@@ -2,7 +2,7 @@ const response = await fetch(
   'https://api.mage.ai/v1/predict',
   {
     body: JSON.stringify({
-      api_key: 'abc_123',
+      api_key: 'G00GWJa65aPbmFKVp9Zv4GvIectoLwKaA5wl1aH5',
       model: 'awesome_house_predictor',
       version: '1',
       features: [{
