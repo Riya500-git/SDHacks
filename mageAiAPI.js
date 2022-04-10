4f9916fd3e05932ea05f4746a62be5484fca2a3a
TRucker20 <83630849+TRucker20@users.noreply.github.com>
2022-04-10 03:02:39 -0700

    Update mageAiAPI.js

--- mageAiAPI.js
+++ mageAiAPI.js
@@ -3,11 +3,10 @@ const response = await fetch(
   {
     body: JSON.stringify({
       api_key: 'G00GWJa65aPbmFKVp9Zv4GvIectoLwKaA5wl1aH5',
-      model: 'awesome_house_predictor',
-      version: '1',
+      model: 'recommendations_rank_1649534003947',
+      version: '3',
       features: [{
-        floor_count: 4,
-        year_built: 1967,
+        id = "AZ"
       }],
     }),
     method: 'POST',
